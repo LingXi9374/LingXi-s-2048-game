4389919178619c1c7e5fb44c2e2d8cd4079a65ad
unknown <yyr1919810@gmail.com>
2025-07-14 22:42:01 +0800

    feat: 修复通知栏无法隐藏的bug

--- app/src/main/java/com/example/lingxis2048/MainActivity.kt
+++ app/src/main/java/com/example/lingxis2048/MainActivity.kt
@@ -8,11 +8,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.lingxis2048.ui.theme.LingXis2048Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Hide the status bar persistently
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
+
         setContent {
             LingXis2048Theme {
                 // A surface container using the 'background' color from the theme
